7569619352fbc8c1a17b36e0d94400fff0319cea
nxtrs2 <simonshareef@gmail.com>
2024-12-09 17:00:11 +0500

    small change

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to Swanky Shears</h1>
+
       <Booking />
     </main>
   );
